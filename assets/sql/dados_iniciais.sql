c818f629207bc6df1afb8a2120bec13da24a6bfa
gabrielbart <bizzottogabriel150@gmail.com>
2021-09-28 10:21:32 +0000

    Preparação da Base de dados

---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+INSERT INTO generos(id, nome) VALUES
+          (1, 'Alta Fantasia'),
+          (2, 'Policial'),
+          (3, 'Drama'),
+          (4, 'Ficção Científica');
+
+    INSERT INTO livros(id, titulo, id_genero) VALUES
+        (1, 'Assassinato no expresso do Oriente', 2),
+        (2, 'A sociedade do Anel', 1),
+        (3, 'Como eu era antes de você',3),
+        (4, 'O Guia do Mochileiro das Galaxias', 4);
