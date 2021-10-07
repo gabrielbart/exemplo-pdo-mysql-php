4880b892647a894046638040e67ae89d8147cd04
gabrielbart <bizzottogabriel150@gmail.com>
2021-10-07 23:53:52 +0000

    boot

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require_once './vendor/autoload.php';
     
     $comando = $bd->prepare('SELECT * FROM generos');
     $comando->execute();
-    
     $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
@@ -17,24 +16,28 @@ require_once './vendor/autoload.php';
     <head>
         <meta charset="utf-8">
         <title>Biblioteca</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
     <body>
-        <a href="insert.php">Novo Gênero</a>
-        <table>
+       <main class="container">
+        <a class="btn btn-primary" href="insert.php">Novo Gênero</a>
+        <table class="table">
             <tr>
                 <th>Id</th>
                 <th>Nome</th>
+                <th>&nbsp;</th>
             </tr>
             <?php foreach($generos as $g): ?>
                 <tr>
                     <td><?=$g['id']?></td>
                     <td><?=$g['nome']?></td>
                     <td>
-                        <a href="update.php?id=<?= $g['id'] ?>">Editar</a>
-                        <a href="delete.php?id=<?= $g['id'] ?>">Excluir</a>
+                        <a class="btn btn-secondary" href="update.php?id=<?= $g['id'] ?>">Editar</a>
+                        <a class="btn btn-danger" href="delete.php?id=<?= $g['id'] ?>">Excluir</a>
                     </td>
                 </tr>
             <?php endforeach ?>
         </table>
+       </main>
     </body>
 </html>
--- insert.php
+++ insert.php
@@ -18,13 +18,20 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <head>
         <meta charset="utf-8"
         <title>Novo Gênero</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
-    <body>
+<body>
+    <main class="container">
         <h1>Novo Gênero</h1>
         <form action="inser.php" method="post">
+            <div class="form-group">
             <label for="nome">Nome do Gênero</label>
-            <input type="text" required name="nome"/>
-            <button type="submit">Salvar</button>
+            <input class="form-control" type="text" name="nome"
+            </div>
+            <br />
+            <a class="btn btn-secondary" href="index.php">Voltar</a>
+            <button class="btn btn-success" type="submit">Salvar</button>
         </form>
-    </body>
+</main>
+</body>
 </html>
