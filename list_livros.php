b7903bf1b18a1bd4eec76a9e7c1fb2584d601fee
gabrielbart <bizzottogabriel150@gmail.com>
2021-10-25 17:49:49 +0000

    list

---
+++ list_livros.php
@@ -0,0 +1,11 @@
+<?php
+require_once './vendor/autoload.php';
+
+use ExemploPDOMySQL\MySQLConnection;
+
+$bd = new MySQLConnection();
+
+$comando - $bd>prepare('SELETC * FROM livros')
+$comando
+
+<?php include('./includes/header.php'); ?>
