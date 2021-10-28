820d28b64448bfb91ccd51d23cd4e42bdf8aa17f
gabrielbart <bizzottogabriel150@gmail.com>
2021-10-28 17:41:16 +0000

    index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once './vendor/autoload.php';
 
 use ExemploPDOMySQL\MySQLConnection; //PDO;
 
-$bd = new MySQLConnection(); //PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');  
+$bd = new MySQLConnection(); //PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
 
 $comando = $bd->prepare('SELECT * FROM generos');
 $comando->execute();
